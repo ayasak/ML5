eacbb3d98feb7a302e87b7fba0745fcf485ce516
ayasak <56744387+ayasak@users.noreply.github.com>
2020-11-29 12:02:30 -1000

    Update posenet1.js

--- posenet1.js
+++ posenet1.js
@@ -3,14 +3,20 @@ let poseNet;
 let poses = [];
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(700, 500);
   video = createCapture(VIDEO);
   video.size(width, height);
 
-  
+  poseNet = ml5.poseNet(video, modelReady);
+
   poseNet.on('pose', function(results) {
     poses = results;
   });
+  video.hide();
+
+}
+function modelReady() {
+  select('#status').html('Model Loaded');
 }
 function draw() {
   image(video, 0, 0, width, height);
@@ -24,10 +30,17 @@ function drawKeypoints()  {
     let pose = poses[i].pose;
     for (let j = 0; j < pose.keypoints.length; j++) {
       let keypoint = pose.keypoints[j];
-      if (keypoint.score > 0.3) {
+      if (keypoint.score > 0.2) {
         fill(255, 255, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+
+        if(pose.keypoints[0].position.y > 400) {
+          document.getElementById('title').innerHTML = "DON'T SLOUCH";
+        }
+        else {
+          document.getElementById('title').innerHTML = "Good";
+        }
       }
     }
   }
@@ -39,8 +52,9 @@ function drawSkeleton() {
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
-      stroke(255, 255, 0);
+      stroke(255, 0, 0);
       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+      strokeWeight(10);
     }
   }
 }
